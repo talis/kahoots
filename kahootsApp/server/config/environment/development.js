a9a1c1a9e37479e15753da0d3dea7b2b46531b66
lauren <lorilew123:@gmail.com>
2015-07-03 10:47:40 +0100

    Generated new route - login

--- kahootsApp/server/config/environment/development.js
+++ kahootsApp/server/config/environment/development.js
@@ -7,6 +7,17 @@ module.exports = {
   mongo: {
     uri: 'mongodb://localhost/kahootsapp-dev'
   },
-
+  oauth: {
+    host: process.env.PERSONA_HOST || 'persona',
+    port: process.env.PERSONA_PORT || 80,
+    scheme: process.env.PERSONA_SCHEME || 'http',
+    route: process.env.PERSONA_ROUTE || '/oauth/tokens/',
+    client: process.env.PERSONA_OAUTH_CLIENT || 'primate',
+    secret: process.env.PERSONA_OAUTH_SECRET || 'bananas',
+    tokenCache: {
+      host: process.env.PERSONA_OAUTH_TOKENCACHE_HOST || 'localhost',
+      port: process.env.PERSONA_OAUTH_TOKENCACHE_PORT || 6379,
+      db: process.env.PERSONA_OAUTH_TOKENCACHE_DB || 0
+    },
   seedDB: true
 };
