e1a353998a6d9a5c40254e8e43b460271a327763
lauren <lorilew123:@gmail.com>
2015-07-10 15:55:48 +0100

    Chrome Extension Login works!

--- kahootsKlipper/app/scripts/background.js
+++ kahootsKlipper/app/scripts/background.js
@@ -2,79 +2,58 @@
 
 // Wait for page to load before running background script.
 var ready = false;
- document.addEventListener("DOMContentLoaded", function(event) {
+var PERSONA_ENDPOINT = "https://users.talis.com";
+var LOGIN_COMPLETE_URL = PERSONA_ENDPOINT + "/auth/login?nc=";
+var isLoggedIn = false;
+var username = null;
+
+document.addEventListener("DOMContentLoaded", function(event) {
    console.log("DOM fully loaded and parsed");
    ready = true;
- });
-
+});
 
   // Listen for messages from popup and content scripts.
-  chrome.extension.onMessage.addListener(
-    function (request, sender, sendResponse) {
-      var userEmail;
+chrome.extension.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    // Login from talis-uploader-desktop-app
 
-      switch (request.directive) {
-        case "popup-click":
-          if(confirm("popup-click!")){
-            /*
-            TODO: check the user is logged in before you start clipping.
-            If logged in - carry on as normal.
-            If not - redirect to google login.
-             */
-            console.log("here");
-            // Get user email
-            chrome.identity.getProfileUserInfo(function(userInfo){
-              if (chrome.runtime.lastError) {
-                alert(chrome.runtime.lastError);
-              } else {
-                userEmail = userInfo.email;
-                // check email is not null or undefined.
-                if(userEmail === undefined || userEmail === null){
-                  if(confirm("You need to sign into google to use Klipper. Do you want to login?")){
-                    var newURL = "chrome://chrome-signin/?source=0";
-                    chrome.tabs.create({ url: newURL });
-                  }
-                }else if(ready) {
-                  if(confirm("You are logged into chrome using " + userEmail + ". Clips will be sent to this user. Is this user you?")) {
-                    // execute the content script that lets the user draw a rectangle
-                   // createCanvas();
-                    startKlipper();
-                  }
-                }
-              }
-              sendResponse({});
-            });
-          }
+
+    switch (request.directive) {
+        case "login":
+          alert("Login clicked");
+          getLoginData(function(user){
+
+            alert("user: " + user.profile.first_name);
+            sendResponse({"status": 200, "name":user.profile.first_name});
+          });
+
 
           break;
-
+        case "popup-click":
+          if(confirm("popup-click!")){
+            // TODO: check user if logged in before clipping.
+            startKlipper();
+            sendResponse({});
+          }
+          break;
         case "capture":
           //Take a screen shot and send to kahoots server
-          chrome.tabs.captureVisibleTab(null, function (img) {
-            var xhr = new XMLHttpRequest();
-            var formData = new FormData();
-            formData.append("content", img);
-            formData.append("rect", JSON.stringify(request.rect));
-            formData.append("source", request.source);
-            formData.append("author", userEmail);
-            xhr.open("POST", "http://localhost:9000/api/clips/file-upload/", true);
-            xhr.send(formData);
-          });
+          screenshot(request);
           sendResponse({msg:"Sent clip to Kahoots App"});
           break;
         default:
           // helps debug when request directive doesn't match
           alert("Unmatched request of '" + request + "' from script to background.js from " + sender);
-      }
     }
-  );
+  }
+);
 
 
 /*
    Allows the user to select part of the screen to clip.
    When finished sends a message "capture" with rectangle dimensions and url.
  */
- function startKlipper(){
+function startKlipper(){
    chrome.tabs.executeScript(null, { // defaults to the current tab
      file: "scripts/canvas.js"
    }, function(){
@@ -82,6 +61,85 @@ var ready = false;
        file: "scripts/rectangle.js"
      });
    });
- }
+}
+function screenshot(request) {
+  chrome.tabs.captureVisibleTab(null, function (img) {
+    var xhr = new XMLHttpRequest();
+    var formData = new FormData();
+    formData.append("content", img);
+    formData.append("rect", JSON.stringify(request.rect));
+    formData.append("source", request.source);
+    //formData.append("author", userEmail);
+    xhr.open("POST", "http://localhost:9000/api/clips/file-upload/", true);
+    xhr.send(formData);
+  });
+}
+
+function getLoginData(callback){
+  try{
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", PERSONA_ENDPOINT+"/2/auth/login.json?cd=JSON_CALLBACK", true);
+    xhr.onreadystatechange = function() {
+      // 4: request finished and response is ready
+      if (xhr.readyState === 4) {
+        var resp = this;
+        if (resp.status === 200) {
+          //alert('*Status: '+this.status+'\nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'\nBody: '+this.responseText);
+          var data = JSON.parse(resp.responseText);
+          if (data) {
+            alert(JSON.stringify(data.profile) + "\n" + data.guid);
+            user = data;
+            username = data.profile.first_name;
+            isLoggedIn = true;
+            callback(user);
+          } else {
+            alert('No data received for user, despite 200 \n');
+            rcallback(user);
+          }
+        }else if(resp.status === 401){
+          alert("User not logged in");
+          user = null;
+          //try login
+          login(PERSONA_ENDPOINT, user);
+        }else{
+          alert('*Status: '+this.status+'\nHeaders: '+JSON.stringify(this.getAllResponseHeaders())+'\nBody: '+this.responseText);
+        }
+      }
+    };
+    xhr.send();
+    alert("Sent Request");
+  }catch(e){
+    alert("Error trying ot get user data");
+  }
+
+}
+
+function login(PERSONA_ENDPOINT, user){
+  if(user !== undefined && user !== null){
+    alert("[login] user is not null:" + "\n" + user);
+    //user is logged in.
+    //get user's GUID
+  }else{
+    alert("[login] user is null, attempt to login");
+    //user is not logged in.
+    // set up the next location which will either use the nextPath or whatever was in rootScope.absUrl if it was specified
+    var nextLocation = PERSONA_ENDPOINT+'/auth/providers/google/login?redirectUri=&nc=' + new Date().getTime();
+
+    chrome.tabs.create( {
+      "url": nextLocation
+    },function(tab) {
+      alert("here");
+      thistabId = tab.id;
+      chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab){
+        if(thistabId===tabId && changeInfo.url.substring(0, LOGIN_COMPLETE_URL.length)===LOGIN_COMPLETE_URL){
+          chrome.tabs.remove(tabId);
+        }
+      });
+    });
 
 
+
+
+
+  }
+}
--- kahootsKlipper/app/scripts/popup.js
+++ kahootsKlipper/app/scripts/popup.js
@@ -1,19 +1,41 @@
-var signin_button, clip_button;
-
-
-
-document.addEventListener('DOMContentLoaded', function () {
-  document.getElementById('clip').addEventListener('click', clip_clickHandler);
-})
-
-
-
+// Handles click events for clip button
+var username;
 
 function clip_clickHandler(e) {
+
   chrome.extension.sendMessage({directive: "popup-click"}, function(response) {
     this.close(); // close the popup when the background finishes processing request
   });
 }
+
+//Handles click events for sign in button.
+function signin_clickHandler(e) {
+  chrome.extension.sendMessage({directive: "login"}, function(response) {
+  });
+  this.close();
+}
+
+var main = function() {
+    document.getElementById('signin').addEventListener('click', signin_clickHandler);
+    document.getElementById('clip').addEventListener('click', clip_clickHandler);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  //check logged in.
+  chrome.runtime.getBackgroundPage(function (backgroundPage) {
+    if(backgroundPage.username !== null) {
+      username = backgroundPage.username;
+      document.getElementById('welcome-msg').innerText = "Hello, " + username;
+    }else{
+      document.getElementById('welcome-msg').innerText = "You need to login!";
+    }
+  });
+
+  main();
+});
+
+
+
 
 
 
