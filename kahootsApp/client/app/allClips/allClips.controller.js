470a9935a70667d9139dda75012ad8046f680dd2
lorilew123 <lorilew123@gmail.com>
2015-07-16 20:55:26 +0100

    Updated allClips controller

--- kahootsApp/client/app/allClips/allClips.controller.js
+++ kahootsApp/client/app/allClips/allClips.controller.js
@@ -5,9 +5,9 @@ angular.module('kahootsAppApp')
     $scope.newComment='';
     $scope.awesomeClips=[];
     $scope.activeClip;
-    $scope.username = $rootScope.user.profile.first_name;
+    $scope.username = $rootScope.user.first_name;
 
-    $http.get('/api/clips/mine/' + $rootScope.user.guid+"?access_token="+$rootScope.oauth.access_token, {headers:  {
+    $http.get('/api/clips/mine/' + $rootScope.user._id+"?access_token="+$rootScope.oauth.access_token, {headers:  {
       'Authorization': 'Bearer ' + $rootScope.oauth.access_token }}).success(function(awesomeClips) {
       $scope.awesomeClips = awesomeClips;
       $scope.activeClip = awesomeClips[0];
@@ -31,7 +31,7 @@ angular.module('kahootsAppApp')
       var id = $scope.activeClip._id;
       $scope.activeClip.comments.push($scope.newComment);
       console.log($scope.activeClip.comments);
-      $http.put('/api/clips/' + id + "?user_id=" + $rootScope.user.guid+"&access_token="+$rootScope.oauth.access_token, {headers:  {
+      $http.put('/api/clips/' + id + "?user_id=" + $rootScope.user._id+"&access_token="+$rootScope.oauth.access_token, {headers:  {
         'Authorization': 'Bearer ' + $rootScope.oauth.access_token }, clip:$scope.activeClip});
       $scope.newComment = '';
     };
